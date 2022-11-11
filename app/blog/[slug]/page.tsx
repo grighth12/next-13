d0c1c9f410921db1357915962904162520364db4
hanna <hanna@grepp.co>
2022-11-11 20:14:04 +0900

    wip: try dynamic title

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,41 @@
+'use client';
 
+import { useEffect } from "react";;
+import Head from 'next/head';
 interface Props {
   params: {slug : string},
   searchParams: {id:string}
-}
+};
 
-const Page =({params, searchParams}: Props) => {
-  return (
-    <div>
-      <h1>Blog Post</h1>
-      <p>Slug: {params.slug}</p>
-      <p>Id: {searchParams.id}</p>
-    </div>);
-}
+const getPost = async (id: string) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const posts = await res.json();
+
+  return posts;
+};
+
+
+const Page = ({params, searchParams}: Props) => {
+  useEffect(() => {
+    const setDocumentTitle = async () => {
+      const post = await getPost(params.slug);       
+      document.title = post.title;
+    }
+
+    setDocumentTitle();
+  }, [params.slug]);
+
+
+  return (<>
+      <Head>
+        <title>Blog Post</title>
+      </Head>
+      <div>
+        <h1>Blog Post</h1>
+        <p>Slug: {params.slug}</p>
+        <p>Id: {searchParams.id}</p>
+      </div>
+    </>);
+};
 
 export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 const Page = () => {
-  return <h1>Hello, Next.js!</h1>
+  return <>
+    <h1>Hello, Next.js!</h1>
+    <Link href="/blog/1">Go to Post 1</Link>
+  </> 
 }
 
 export default Page;
